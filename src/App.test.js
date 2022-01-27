07cd282518dda9da05b0faa710e1da6eb7398251
EddieJorden <eddie@eddiejorden.tech>
2022-01-26 18:09:05 -0800

    cleaned up a few things

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,6 @@ import App from './App';
 import React from 'react';
 import { shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
-
 describe('App is rendering', function () {
   it('should render "hello world"', function () {
     const wrapper = shallow(<App/>);
