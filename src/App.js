3006f2415b735075d81455bdf6b145fb1bf3f565
EddieJorden <eddie@eddiejorden.tech>
2022-01-26 18:03:14 -0800

    setup enzyme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import React from "react";
 import './App.css';
 import SearchBar from './components/searchbar/SearchBar';
 import SearchBarReturn from './components/searchbar/SearchBarReturn';
 import RedditReturn from './components/reddit/RedditReturn';
 
 function App() {
+  const greeting = "hello world"
+
   return (
     <div className="App">
       <header className="App-header">
-        Hello World
+      <h1>{greeting}</h1>
+        <div>
           <SearchBarReturn />
           <SearchBar />
           <RedditReturn />
+        </div>
       </header>
     </div>
   );
