ab00135c2684e2ede8b68217df8bb49192f51fb9
EddieJorden <eddie@eddiejorden.tech>
2022-03-22 18:53:50 -0700

    added button which hits reddit api and log's the results

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,17 +1,25 @@
 // import { useState } from "react";
 import { debounce } from "lodash";
 import { updateUserInput } from './searchBarSlice';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectRedditReturn } from "../reddit/RedditSlice";
 
 const SearchBar = () => {
     const dispatch = useDispatch();
+    const redditReturnedFromClick = useSelector(selectRedditReturn)
     const handleChange = (event) => {
         dispatch(updateUserInput(event.target.value));
     };
 
+    const handleClick = () => {
+        console.log(redditReturnedFromClick)
+
+    }
+
     return (
         <div>
             <input placeholder='search' type='text' onChange={debounce(handleChange, 1000)}/>
+            <button onClick={handleClick}>hit api</button>
         </div>
     );
 };
